bf404ab4fc93aa8335cc1602890a5c31ec0c306a
Thibaud Clairet <166002489+Moutonmalpoli@users.noreply.github.com>
2025-05-15 14:28:16 +0200

    Add files via upload

---
+++ Bouteille.cs
@@ -0,0 +1,115 @@
+﻿namespace ClassLibraryBouteille
+{
+    public class Bouteille
+    {
+        private decimal contenanceEnLitre;
+        private decimal contenuEnLitre;
+        private Boolean estOuverte;
+
+
+
+        //constructeur par defaut
+        public Bouteille() : this(false, 1, 1)
+        {
+
+        }
+
+        //constructeur classique
+        public Bouteille(bool estOuverte, decimal contenuEnLitre, decimal contenanceEnLitre)
+        {
+            if (contenuEnLitre <= 0)
+            {
+                throw new ArgumentException("Le contenuEnLitre d'une bouteille ne peut être que positif", nameof(contenuEnLitre));
+            }
+
+
+            this.estOuverte = estOuverte;
+            this.contenuEnLitre = contenuEnLitre;
+            this.contenanceEnLitre = contenanceEnLitre;
+
+
+
+        }
+
+        //constructeur par clonage
+        public Bouteille(Bouteille bouteilleACopier) : this(false, 1, 1)
+        {
+            //this.estOuverte = bouteilleACopier.estOuverte;
+            //this.contenanceEnLitre = bouteilleACopier.contenanceEnLitre;
+            //this.contenuEnLitre = bouteilleACopier.contenuEnLitre;
+        }
+
+
+
+        public override string ToString()
+        {
+            return base.ToString() + " estOuverte= " + estOuverte + " contenanceEnLitre= " + contenanceEnLitre + " contenuEnLitre= " + contenuEnLitre;
+        }
+
+        public bool Ouvrir()
+        {
+            if (this.estOuverte == false)
+            {
+                this.estOuverte = true;
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+
+        }
+        public bool Remplir(decimal quantiteARemplir)
+        {
+            if (quantiteARemplir <= 0)
+            {
+                throw new ArgumentException("La quantiteARemplir d'une bouteille ne peut être que positif", nameof(quantiteARemplir));
+            }
+            if (this.contenuEnLitre + quantiteARemplir <= this.contenanceEnLitre && this.estOuverte == true)
+            {
+                this.contenuEnLitre += quantiteARemplir;
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+        public bool Vider(decimal quantiteAVider)
+        {
+            if (quantiteAVider <= 0)
+            {
+                throw new ArgumentException("La quantiteAVider d'une bouteille ne peut être que positif", nameof(quantiteAVider));
+            }
+            if (this.contenuEnLitre - quantiteAVider >= 0 && this.estOuverte == true)
+            {
+                this.contenuEnLitre -= quantiteAVider;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool Transferer(Bouteille bouteilleDestination, decimal quantiteATransferer)
+        {
+            if (this.estOuverte == true && bouteilleDestination.estOuverte == true && this.contenuEnLitre - quantiteATransferer >= 0
+                && bouteilleDestination.contenuEnLitre + quantiteATransferer <= bouteilleDestination.contenanceEnLitre)
+            {
+                this.contenuEnLitre -= quantiteATransferer;
+                bouteilleDestination.contenuEnLitre += quantiteATransferer;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using ClassLibraryBouteille;
+namespace BouteilleObjet
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Bouteille a = new Bouteille(true, 1, 3);
+            a.Remplir(2);
+            a.Vider(-3);
+            Console.WriteLine("Bouteille a: " + a.ToString());
+
+            Bouteille b = new Bouteille(false, 3, 3);
+            b.Ouvrir();
+            Console.WriteLine("Bouteille b: " + b.ToString());
+
+            Bouteille c = new Bouteille(true, 5, 5);
+            c.Vider(5);
+            c.Remplir(2);
+            b.Transferer(c, 3);
+            Console.WriteLine("Bouteille c: " + c.ToString());
+
+            Console.WriteLine("Bouteille b après Transferer: " + b.ToString());
+
+
+
+        }
+
+
+
+
+    }
+
+}
+
